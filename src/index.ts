3adab328d19478d27f4fd2fc6d4badbdc60db59a
HuyNQ <nqh.ktpm.k62@gmail.com>
2023-06-25 16:31:11 +0700

    ,

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { Router } from "./router.ts/Router";
+import path from "path";
 
 dotenv.config();
 
@@ -14,16 +16,26 @@ if (!process.env.PORT) {
  const app = express();
 
 app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'", "example.com"],
+        imgSrc: ["'self'", "https://s3-alpha-sig.figma.com"],
+      },
+    },
+  })
+);
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
 
-
-
-//app.use("/api/menu/items", itemsRouter);
+app.use("/", Router);
 app.set("view engine","ejs");//set view engine
 app.set("views","./src/view")//chi ra thu muc chua view
   
--- src/router.ts/Router.ts
+++ src/router.ts/Router.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express,{Request,Response} from "express";
 
 export const Router = express.Router();
 
-Router.get("/",)
+Router.get("/", async (req: Request, res: Response)=>{
+    res.render("index")
+})
